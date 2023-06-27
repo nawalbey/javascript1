a48b0127288b4f4662aa29b6ecbf6ba829838917
nawal <nbey34@gmail.com>
2023-06-27 16:26:18 +0200

    exercice javascript

---
+++ index.js
@@ -0,0 +1,40 @@
+var largeur = 30;
+var longeur = 40;
+var hauteur = 50;
+
+var volume = 0;
+
+var volume = largeur * longeur * hauteur;
+console.log(volume);
+
+let age = 2;
+// si age est strictement inferieur a 5 afficher le mot bébé
+// si age est compris entre 5 et 10 afficher le mot enfant
+// si age est compris entre 11 et 18 afficher le mot ado
+// si age est superieur a 18 afficher le mot vieux
+
+if (age < 5){
+    console.log("bébé");
+} 
+
+if ((age > 5 && age< 10)){
+    console.log("enfant");
+} 
+if ((age > 11 && age < 18)){
+console.log("age");
+
+}
+if (age > 18) {
+console.log("vieux");
+}
+
+// en utilisant la structure conditionnelle switch
+// ecrire un programme afficher le jour de la semain en fonction d'un nombre donné
+// si un nombre =0 afficher dimanche
+
+// en utilisanr la ternaire ecrire un programme qui affiche adulte si l'age est superieur ou a 18 et mineur si age est inferieur a 18
+// une 
+
+if  jourSemaine[
+    
+]
