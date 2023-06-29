0081998bacb02403751e2d16c12f0d13ba8b45b9
nawal <nbey34@gmail.com>
2023-06-29 16:49:09 +0200

    exercice 2 javascript

--- exercice_function_js/exercicefuntion1.js
+++ exercice_function_js/exercicefuntion1.js
@@ -7,7 +7,7 @@
 //example
 // maxmin (2, 5, true) => 5
 // maxmin (2, 5, false) => 2
- 
+
 // // Exo 1
 // functionmaxMin(a, b, c=true) {​​​​​​​​
 // if (c==true) {​​​​​​​​ // si c == true
@@ -16,7 +16,7 @@
 //  }​​​​​​​​ else {​​​​​​​​ // sinon
 // returnb; // on retourne b
 //  }​​​​​​​​
-//  }​​​​​​​​ 
+//  }​​​​​​​​
 // else {​​​​​​​​ // si c = false
 // if (a<b) {​​​​​​​​ // si a est plus petit que b
 // returna; // on retourne a
@@ -27,7 +27,7 @@
 // }​​​​​​​​
 // letresultat=maxMin(3, 8, true);
 // console.log(resultat);
- 
+
 // // on retourne a si (c = true et a > b) ou (c = false et a < b)
 // // on retourne b si (c = true et a < b) ou (c = false et a > b)
 // functionmaxMin(a, b, c=true) {​​​​​​​​
@@ -37,11 +37,98 @@
 // returnb;
 //  }​​​​​​​​
 // }​​​​​​​​
- 
-letresult=maxMin(3, 8, false);
+
+letresult = maxMin(3, 8, false);
 console.log(result);
 
-function sommeTab[1, 2, 3, 4, 5,]{
-    
+function sommeTab(tableau) {
+  let somme = 0;
 
+  let i = 0;
+  while (i < tableau.length) {
+    somme = somme + tableau[i];
+    i++;
+  }
+  return somme;
 }
+
+function maxTab(tableau) {
+  let max = tableau[0]; // on considere le premier elemement comme etant le plus grand
+  for (let i = 0; i < tableau.length; i++) {
+    // pour i allant de 0 a la taille du tableau
+    // comparer max de la valeur a la position i du tableau
+    if (max < tableau[i]) {
+      // si max est plus petit
+      // on remplace la valeur de max par la valeur de la position i du tableau
+      max = tableau[i];
+    }
+  }
+  return max; // retourner max
+}
+
+let tab1 = [5, 8, 3, 0];
+let resultat3 = maxTab(tab1);
+console.log(resultat3); // affiche => 8
+
+function maxMinTab(tableau, c) {
+  if (c == true) {
+    return maXtab(tableau);
+  }
+}
+
+function maxTab(tableau) {
+  let max = tableau[0]; // on considere le premier elemement comme etant le plus grand
+  for (let i = 0; i < tableau.length; i++) {
+    // pour i allant de 0 a la taille du tableau
+    // comparer max de la valeur a la position i du tableau
+    if (max < tableau[i]) {
+      // si max est plus petit
+      // on remplace la valeur de max par la valeur de la position i du tableau
+      max = tableau[i];
+    }
+  }
+  return max; // retourner max
+}
+
+function minTab(tableau) {
+  let min = tableau[0]; // on considere le premier elemement comme etant le plus grand
+  for (let i = 0; i < tableau.length; i++) {
+    // pour i allant de 0 a la taille du tableau
+    // comparer max de la valeur a la position i du tableau
+    if (min > tableau[i]) {
+      // si max est plus petit
+      // on remplace la valeur de max par la valeur de la position i du tableau
+      min = tableau[i];
+    }
+  }
+  return min; // retourner max
+}
+
+function maxMinTab(tableau, c) {
+  if (c == true) {
+    return maxTab(tableau);
+  } else {
+    return minTab(tableau);
+  }
+}
+
+//tableau.sort il trie et mit il met du plus petit au plus grand
+
+function reverseWord(str) {
+  let tab = str.split(""); // conversion de la chaine
+  tab.reverse(); //inverser le tableau
+  let chaine = tab.join(""); // convertir le tableau en chaine de caractere
+  return chaine;
+}
+
+console.log(reverseWord("hello world"));
+
+// soit la variabl note
+//en utilisant la structure conditionnelle ternaire
+//ecrire un code qui permet d'afficher le message bien si note est superieur ou egal a 15
+// mauvais si note est strictement inferieur a 15
+
+function notation(nb) {
+  nb >= 15 ? console.log("tres bien") : console.log("mauvais");
+}
+notation(11)
